44c4ee056de8b43ef1420825832c1569b8c1438e
Douglas Berdeaux <weaknetlabs@gmail.com>
2022-09-22 10:19:17 -0400

    Removed record handler
    context now shows members properly

--- modules/list-da-members.py
+++ modules/list-da-members.py
@@ -1,9 +1,8 @@
 from ldap3 import Server, Connection, NTLM, ALL
-#import sys ## DEBUG
 
 class CMEModule:
     '''
-        List out all Domain Admin members
+        List out all Domain Admin Accounts
         Module by Douglas Berdeaux (@RackunSec)
     '''
     name = 'list-da-members'
@@ -12,26 +11,12 @@ class CMEModule:
     opsec_safe = True #Does the module touch disk?
     multiple_hosts = True # Does it make sense to run this module on multiple hosts at a time?
 
-
-
     def options(self, context, module_options):
         self.context = context
 
-    def process_record(self, item):
-        if not isinstance(item, ldapasn1.SearchResultEntry):
-            return
-        else:
-            try:
-                for attribute in item['attributes']:
-                    if str(attribute['type']) == 'sAMAccountName':
-                        sAMAccountName = attribute['vals'][0].asOctets().decode('utf-8')
-                        print(f"Domain Admin: {sAMAccountName}")
-            except Exception as e:
-                print(f"[DAFUQ]: {e}")
 
     def on_login(self, context, connection):
         searchBase = connection.ldapConnection._baseDN
-        #sys.exit() ## DEBUG
 
         searchFilter='(memberOf=CN=Domain Admins,OU=Groups,'+connection.baseDN+')'
 
@@ -39,9 +24,14 @@ class CMEModule:
 
         try:
             sc = ldap.SimplePagedResultsControl()
-            connection.ldapConnection.search(searchFilter=searchFilter,
+            records = connection.ldapConnection.search(searchFilter=searchFilter,
                 attributes=['sAMAccountName', 'description'],
-                sizeLimit=999, searchControls=[sc],
-                perRecordCallback=self.process_record)
+                sizeLimit=999, searchControls=[sc])
+            for record in records:
+                if isinstance(record,ldapasn1.SearchResultEntry):
+                    for attribute in record['attributes']:
+                        if str(attribute['type']) == 'sAMAccountName':
+                            sAMAccountName = attribute['vals'][0].asOctets().decode('utf-8')
+                            context.log.success(f"Domain Admin: {sAMAccountName}")
         except LDAPSearchError as e:
             context.log.error('Obtained unexpected exception: {}'.format(str(e)))
